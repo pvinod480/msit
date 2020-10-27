4d9b0d521eb53d93d97d0ea3875cbe5670ad2425
pvinod480 <pvinod480@gmail.com>
2020-10-27 11:37:22 +0530

    Python file

---
+++ Happy.py
@@ -0,0 +1,12 @@
+while True:
+    a=input("Do you have a problem in life..? \n")
+    if a == "yes":
+        b=input("Can you Do Something about it..? \n")
+        if b=="yes" or b=="no":
+            print("THEN WHY WORRY ?")
+    else:
+        print("THEN WHY WORRY ?")
+    print("---------------------------------------------\n")
+    print()
+
+    
